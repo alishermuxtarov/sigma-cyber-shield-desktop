e2bd3c204563f3fe0bc684f2d5eb7983b10ef919
GoTLiuM <gotlium@gmail.com>
2024-01-28 12:12:13 +0500

    fixes and updates

--- rewrite.py
+++ rewrite.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/Users/ruslanaskarov/.virtualenvs/sigma-cyber-shield-desktop/bin/python
 
 import re
 import os
@@ -6,13 +6,19 @@ import sys
 import logging
 import traceback
 import hashlib
+import json
 from urllib.parse import urlparse
+import urllib3
+import warnings
 
 import requests
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import mysql.connector
 
+
+urllib3.disable_warnings()
+warnings.filterwarnings("ignore")
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -32,10 +38,25 @@ STOP_URL = 'http://127.0.0.1:8000/stop.html'
 API_URL = 'https://odc24.rbc-group.uz/api'
 
 NOTIFICATION_TEMPLATE = '''
-osascript -e 'display notification "{}" with title "{}"'
+osascript -e 'display notification "{}" with title "{}" sound name "Glass"'
 '''
 
-BLOCK_URL = {
+_BLOCK_CONTENT = {
+    'Payme': {
+        'level': 'critical',
+        'title': 'Стоп мошенник',
+        'message': 'Доступ к контенту ограничен. Так как содержит опасный контент!',
+        'block': True,
+        'domain': 'payme.uz'
+    },
+    'sexX': {
+        'level': 'critical',
+        'title': 'Стоп мошенник',
+        'message': 'Доступ к контенту ограничен. Контент сексуального характера!',
+        'block': True,
+    },
+}
+_BLOCK_URL = {
     'yandex.ru': {
         'level': 'info',
         'title': 'Предупреждение',
@@ -50,33 +71,50 @@ BLOCK_URL = {
     },
 }
 
-BLOCK_CONTENT = {
-    'Payme': {
-        'level': 'critical',
-        'title': 'Стоп мошенник',
-        'message': 'Доступ к контенту ограничен. Так как содержит опасный контент!',
-        'block': True,
-        'domain': 'payme.uz'
-    },
-    'sex': {
-        'level': 'critical',
-        'title': 'Стоп мошенник',
-        'message': 'Доступ к контенту ограничен. Контент сексуального характера!',
-        'block': True,
-    },
-}
-
 MEDIA_TYPES = [
     'swf', 'ogg', 'ogv', 'svg', 'svgz', 'eot', 'otf', 'woff', 'mp4', 'ttf', 'css', 'rss', 'atom', 'js', 'jpg', 'jpeg',
     'gif', 'png', 'ico', 'zip', 'tgz', 'gz', 'rar', 'bz2', 'doc', 'xls', 'exe', 'ppt', 'tar', 'mid', 'midi', 'wav',
     'bmp', 'rtf'
 ]
 
-RE = re.compile('|'.join(BLOCK_URL.keys()))
+
+def get_hosts():
+    r = requests.get('https://odc24.rbc-group.uz/api/hosts/', timeout=3)
+    if r.status_code == 200:
+        text = r.content.decode('utf-8', 'ignore').strip()
+        logger.info('Hosts loaded ...')
+    else:
+        text = open('{}/hosts.json'.format(PATH)).read().strip()
+    h = [d['url'] for d in json.loads(text.replace('|', ''))]
+    return '|'.join(h)
 
 
-def update_blacklist():
-    return BLOCK_URL
+def get_keywords():
+    content = {}
+    r = requests.get('https://odc24.rbc-group.uz/api/keywords/', timeout=3)
+    if r.status_code == 200:
+        text = r.content.decode('utf-8', 'ignore').strip()
+        logger.info('Keywords loaded ...')
+    else:
+        text = open('keywords.json').read()
+    for d in json.loads(text):
+        w = d['keyword']
+        content[w] = {
+            'level': 'critical',
+            'title': 'Стоп мошенник',
+            'message': 'Доступ к контенту ограничен. Контент сексуального характера!',
+            'block': True,
+            'domain': d['allowed_for_host'],
+        }
+    return content
+
+
+RE = re.compile(get_hosts())
+BLOCK_CONTENT = get_keywords()
+
+
+def is_media(url):
+    return any(map(lambda x: '.' + x in url, MEDIA_TYPES))
 
 
 def get_page(url):
@@ -84,12 +122,13 @@ def get_page(url):
         if not url.startswith('http'):
             return ''
 
-        if any(map(lambda x: '.' + x in url, MEDIA_TYPES)):
+        if is_media(url):
             return ''
 
         h = requests.head(url, allow_redirects=True, verify=False, timeout=3)
         # only HTML pages
-        if not h.headers['Content-Type'].startswith('text/html'):
+        ct = h.headers['Content-Type']
+        if not ct.startswith('text/html') and not ct.startswith('application/json'):
             return ''
 
         logger.info('>>> {}'.format(url))
@@ -145,7 +184,6 @@ def get_sphinx_connection():
 
 def write_content(url, content):
     if content and content is not None and content.strip():
-        content = text_from_html(content)
         filename = '{}/history/{}.html'.format(PATH, md5(url))
         # Write to history file
         if content is not None and content.strip():
@@ -176,36 +214,42 @@ def main():
         [ch_id, url, ip_addr, http_method, user_login] = request.split()
 
         parse = urlparse(url)
-        # logger.info(parse.netloc)
-
-        logger.info(request)
 
         response = ch_id + ' OK'
-        find_urls = RE.findall(url)
 
-        content = get_page(url)
+        if not is_media(url) and parse.netloc and http_method == 'GET':
+            logger.info(request)
 
-        write_content(url, content)
+            content = get_page(url)
 
-        # find_words = any(map(lambda x: x in content, BLOCK_CONTENT.keys()))
-        # find_words = any(map(lambda x: x in content, BLOCK_CONTENT.keys()))
-        find_words = [w for w in BLOCK_CONTENT.keys() if w in content]
-        if find_urls or find_words:
-            if find_words:
-                data = BLOCK_CONTENT.get(find_words[0])
-                domain = data.get('domain')
-                if domain and parse.netloc == domain:
-                    # ignore site
-                    pass
-                else:
-                    if data.get('block'):
-                        response += ' status=301 url={}'.format(STOP_URL)
-                    notify(data.get('title'), data.get('message'))
-            elif find_urls:
-                data = BLOCK_URL.get(find_urls[0])
-                if data.get('block'):
-                    response += ' status=301 url={}'.format(REWRITE_URL)
-                notify(data.get('title'), data.get('message'))
+            if content:
+                visible_content = text_from_html(content).lower()
+                write_content(url, visible_content)
+
+                find_urls = RE.findall(url)
+
+                # find_words = any(map(lambda x: x in content, BLOCK_CONTENT.keys()))
+                # find_words = any(map(lambda x: x in content, BLOCK_CONTENT.keys()))
+                find_words = [w for w in BLOCK_CONTENT.keys() if w in visible_content]
+                if find_urls or find_words:
+                    if find_words:
+                        data = BLOCK_CONTENT.get(find_words[0])
+                        domain = data.get('domain')
+                        if domain and parse.netloc == domain:
+                            # ignore site
+                            pass
+                        else:
+                            if data.get('block'):
+                                response += ' status=301 url={}'.format(STOP_URL)
+                            notify(data.get('title'), data.get('message'))
+                            logger.info('[Block by Word] {}'.format(find_words[0]))
+                    elif find_urls:
+                        #data = BLOCK_URL.get(find_urls[0])
+                        #if data.get('block'):
+                        #    response += ' status=301 url={}'.format(REWRITE_URL)
+                        logger.info('[Block by URL] {}'.format(url))
+                        response += ' status=301 url={}'.format(REWRITE_URL)
+                        notify('Опасный сайт', 'Сайт был заблокирован. Так как содержит опасный контент!')
 
         response += '\n'
         sys.stdout.write(response)
@@ -217,5 +261,5 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as ex:
-        logging.fatal(traceback.format_exc())
-        print(ex.__str__())
+        logging.error(traceback.format_exc())
+        # print(ex.__str__())
